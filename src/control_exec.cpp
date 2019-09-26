41961bcff63f72d5e506e20e54e42c9d43d81a21
pohzhiee <poh140996@hotmail.com>
2019-09-26 15:26:34 +0800

    Increase readability for control_exec and pid tuning

--- src/control_exec.cpp
+++ src/control_exec.cpp
@@ -57,114 +57,88 @@ int main(int argc, char **argv)
     // Get all the controllers to be added
     std::vector<std::pair<std::string, std::string>> controllers = {};
     unsigned int controllerSpinCount = 0;
-    if (param_client->service_is_ready())
+    constexpr unsigned int maxSpinCount = 10;
+    while (controllerSpinCount < maxSpinCount)
     {
-        while (controllerSpinCount < 6)
+        if (!param_client->service_is_ready())
         {
-            auto req = std::make_shared<parameter_server_interfaces::srv::GetControllers::Request>();
-            req->robot = robotName;
-            auto resp = param_client->async_send_request(req);
-            RCLCPP_INFO(get_param_node->get_logger(), "(main exec) Sending async request... ...");
-            auto spin_status = rclcpp::spin_until_future_complete(get_param_node, resp, 10s);
-            if (spin_status == rclcpp::executor::FutureReturnCode::SUCCESS)
-            {
-                auto status = resp.wait_for(1s);
-                if (status == std::future_status::ready)
-                {
-                    auto res = resp.get();
-                    auto controllerNames = res->controllers;
-                    auto controllerTypes = res->controller_types;
-                    if (controllerNames.size() != controllerTypes.size())
-                    {
-                        RCLCPP_ERROR(get_param_node->get_logger(),
-                                     "Number of controller names and types don't match, controller name count: %u, type count: %u",
-                                     controllerNames.size(), controllerTypes.size());
-                        return -1;
-                    }
-                    for (size_t i = 0; i < controllerNames.size(); i++)
-                    {
-                        auto name = controllerNames[i];
-                        auto type = controllerTypes[i];
-                        auto pair = std::make_pair(name, type);
-                        controllers.push_back(pair);
-                    }
-                    // Break out of the retrying loop if getting controllers is successful
-                    break;
-                }
-                else
-                {
-                    RCLCPP_ERROR(get_param_node->get_logger(), "GetControllers service failed to execute");
-                    controllerSpinCount++;
-                }
-            }
-            else
-            {
-                RCLCPP_ERROR(get_param_node->get_logger(), "GetControllers service failed to execute (spin failed)");
-                controllerSpinCount++;
-            }
+            controllerSpinCount++;
+            RCLCPP_ERROR(get_param_node->get_logger(),
+                         "GetControllers service failed to start, check that parameter server is launched, retries left: %d", maxSpinCount - controllerSpinCount);
+            continue;
         }
-    }
-    else
-    {
-        RCLCPP_ERROR(get_param_node->get_logger(), "GetControllers service failed to start, check that parameter server is launched");
+
+        auto req = std::make_shared<parameter_server_interfaces::srv::GetControllers::Request>();
+        req->robot = robotName;
+        auto resp = param_client->async_send_request(req);
+        auto spin_status = rclcpp::spin_until_future_complete(get_param_node, resp, 1s);
+        if (spin_status != rclcpp::executor::FutureReturnCode::SUCCESS)
+        {
+            controllerSpinCount++;
+            RCLCPP_ERROR(get_param_node->get_logger(),
+                         "GetControllers service failed to execute (spin failed), retries left: %d", maxSpinCount - controllerSpinCount);
+            continue;
+        }
+
+        auto status = resp.wait_for(1s);
+        if (status != std::future_status::ready)
+        {
+            controllerSpinCount++;
+            RCLCPP_ERROR(get_param_node->get_logger(),
+                         "GetControllers service failed to execute, retries left: %d", maxSpinCount - controllerSpinCount);
+            continue;
+        }
+
+        auto res = resp.get();
+        auto controllerNames = res->controllers;
+        auto controllerTypes = res->controller_types;
+        if (controllerNames.size() != controllerTypes.size())
+        {
+            RCLCPP_ERROR(get_param_node->get_logger(),
+                         "Number of controller names and types don't match, controller name count: %u, type count: %u",
+                         controllerNames.size(), controllerTypes.size());
+            return -1;
+        }
+        for (size_t i = 0; i < controllerNames.size(); i++)
+        {
+            auto name = controllerNames[i];
+            auto type = controllerTypes[i];
+            auto pair = std::make_pair(name, type);
+            controllers.push_back(pair);
+        }
+        // Break out of the retrying loop if getting controllers is successful
+        break;
     }
     for (auto &c : controllers)
     {
         RCLCPP_INFO(get_param_node->get_logger(), "Controller: %s (%s)", c.first.c_str(), c.second.c_str());
-    }
-
-    for (auto &pair : controllers)
-    {
-        cm.load_controller("ros_controllers", pair.second, pair.first);
+        cm.load_controller("ros_controllers", c.second, c.first);
     }
 
     // there is no async spinner in ROS 2, so we have to put the spin() in its own thread
     auto future_handle = std::async(std::launch::async, spin, executor);
 
-    // we can either configure each controller individually through its services
-    // or we use the controller manager to configure every loaded controller
-    unsigned int configureCount = 0;
-    while (configureCount < 5)
+    auto result = cm.configure();
+    if (result != controller_interface::CONTROLLER_INTERFACE_RET_SUCCESS)
     {
-        // Note that while there is a retrying loop here, often when it runs things will fail because of conflicts in controller states
-        // This is probably due to the controllers not taking into account previous state when configuring and etc.
-        auto result = cm.configure();
-        if (result != controller_interface::CONTROLLER_INTERFACE_RET_SUCCESS)
-        {
-            std::cerr << '[' << configureCount << ']' << " At least one controller failed to configure, retrying...";
-            RCUTILS_LOG_ERROR("[%d] At least one controller failed to configure, retrying...", configureCount);
-            configureCount++;
-            cm.cleanup();
-        }
-        else
-        {
-            RCLCPP_INFO(get_param_node->get_logger(), "Controllers finished configuring");
-            break;
-        }
-        std::this_thread::sleep_for(1s);
+        RCUTILS_LOG_ERROR("At least one controller failed to configure");
+        return -1;
     }
-    // and activate all controller
-    unsigned int activateCount = 0;
-    while (activateCount < 6)
+    else
     {
-        // Note that while there is a retrying loop here, often when it runs things will fail because of conflicts in controller states
-        // This is probably due to the controllers not taking into account previous state when activating and etc.
-        if (cm.activate() != controller_interface::CONTROLLER_INTERFACE_RET_SUCCESS)
-        {
-            std::cerr << '[' << activateCount << ']' << " At least one controller failed to configure, retrying...";
-            RCUTILS_LOG_ERROR("[%d] At least one controller failed to activate, retrying...", activateCount);
-            activateCount++;
-            cm.deactivate();
-        }
-        else
-        {
-            RCLCPP_INFO(get_param_node->get_logger(), "Controllers finished activating");
-            break;
-        }
+        RCUTILS_LOG_INFO("Controllers finished configuring");
+    }
+
+    if (cm.activate() != controller_interface::CONTROLLER_INTERFACE_RET_SUCCESS)
+    {
+        RCUTILS_LOG_ERROR("At least one controller failed to activate, retrying...");
+        return -1;
+    }
+    else
+    {
+        RCUTILS_LOG_INFO("Controllers finished activating");
     }
     hardware_interface::hardware_interface_ret_t ret;
-    // Not sure about next line, but was done to make sure things finish before continuing into main loop
-    rclcpp::spin_some(get_param_node);
     // Main Loop
     while (active && rclcpp::ok())
     {
